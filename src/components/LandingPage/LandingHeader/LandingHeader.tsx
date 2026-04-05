bc18c3c19b597b2283ab43da82df56ed197c34d7
Rana Fahad <ranafahadbh@gmail.com>
2026-04-05 17:31:31 +0200

    Login page is now accessible from landing page, and vice versa

--- src/components/LandingPage/LandingHeader/LandingHeader.tsx
+++ src/components/LandingPage/LandingHeader/LandingHeader.tsx
@@ -1,10 +1,11 @@
 import { UnstyledButton } from '@mantine/core';
 import classes from './LandingHeader.module.css';
+import { Link } from 'react-router-dom';
 
 export function LandingHeader() {
   return (
     <header className={classes.header}>
-      <UnstyledButton className={classes.loginButton}>Log in / Sign up</UnstyledButton>
+      <UnstyledButton component={Link} to='/login' className={classes.loginButton}>Log in / Sign up</UnstyledButton>
 
       {/* Signup commented out for now since both were going to lead to the same oauth page anyway */}
       {/* <UnstyledButton className={classes.signupButton}>Sign up</UnstyledButton> */}
